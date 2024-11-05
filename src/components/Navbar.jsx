f3ca5bef250973165764d0571f792dca233b036f
Mojammel Noman <molla.noman23@gmail.com>
2024-11-06 01:41:31 +0600

    add flowbite-react & navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,71 @@
+import { Avatar, Dropdown, Navbar } from "flowbite-react";
+import { Link } from "react-router-dom";
+
+export default function NavComponent() {
+  const links = [
+    {
+      path: "/",
+      name: "Home",
+    },
+    {
+      path: "/blogs/add",
+      name: "Add a Blog",
+    },
+    {
+      path: "/blogs/all",
+      name: "All Blogs",
+    },
+    {
+      path: "/blogs/featured",
+      name: "Featured Blog",
+    },
+    {
+      path: "/blogs/bookmark",
+      name: "Bookmark",
+    },
+  ];
+  return (
+    <Navbar fluid rounded className="bg-slate-950 px-0">
+      <Navbar.Brand href="/">
+        <h2 className="text-xl md:text-2xl" style={{ fontStyle: "italic" }}>
+          Blog Haat
+        </h2>
+      </Navbar.Brand>
+
+      <Navbar.Collapse>
+        {links.map((link) => (
+          <Link
+            key={link.path}
+            to={link.path}
+            className="hover:text-slate-400 duration-300"
+          >
+            {link.name}
+          </Link>
+        ))}
+      </Navbar.Collapse>
+
+      <div className="flex md:order-2">
+        <Dropdown
+          arrowIcon={false}
+          inline
+          label={
+            <Avatar
+              alt="User settings"
+              img="https://flowbite.com/docs/images/people/profile-picture-5.jpg"
+              rounded
+            />
+          }
+        >
+          <Dropdown.Header>
+            <span className="block text-sm">Bonnie Green</span>
+            <span className="block truncate text-sm font-medium">
+              name@flowbite.com
+            </span>
+          </Dropdown.Header>
+          <Dropdown.Item>Sign out</Dropdown.Item>
+        </Dropdown>
+        <Navbar.Toggle />
+      </div>
+    </Navbar>
+  );
+}
--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,10 +1,11 @@
 import { Outlet } from "react-router-dom";
+import NavComponent from "../components/Navbar";
 
 export default function RootLayout() {
   return (
     <div className="container mx-auto px-3 flex flex-col min-h-screen">
       <div className="flex-grow">
-        <h1>Navbar</h1>
+        <NavComponent />
 
         <Outlet />
       </div>
