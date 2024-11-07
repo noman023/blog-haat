c16fd929840011e462760fc43d9191574e0ebca6
Mojammel Noman <molla.noman23@gmail.com>
2024-11-08 02:01:00 +0600

    add not found page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,3 +1,14 @@
+import { Button } from "flowbite-react";
+import { Link } from "react-router-dom";
+
 export default function NotFound() {
-  return <div>Page Not Found </div>;
+  return (
+    <div className="h-screen flex flex-col gap-6 justify-center items-center">
+      <h1 className="text-3xl">Page not found :(</h1>
+
+      <Link to="/">
+        <Button color="gray">Back to home</Button>
+      </Link>
+    </div>
+  );
 }
