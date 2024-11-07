7364b69a07bd7d5844d8edfc0f0e0357d87ddc96
Mojammel Noman <molla.noman23@gmail.com>
2024-11-07 17:57:36 +0600

    add register page

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,3 +1,102 @@
-export default function Register() {
-  return <div>Register</div>;
+import { Button, FileInput, Label, TextInput } from "flowbite-react";
+import { Link } from "react-router-dom";
+import { FcGoogle } from "react-icons/fc";
+
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+import { useState } from "react";
+
+export default function Login() {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const onSubmit = (data) => {};
+
+  return (
+    <div>
+      <form className="bg-slate-700 flex max-w-md flex-col gap-4 border-2 border-gray-500 mx-auto my-10 p-5 rounded-xl shadow-2xl">
+        <div>
+          <div className="mb-2 block">
+            <Label
+              htmlFor="name"
+              value="Your Name"
+              className="text-slate-300"
+            />
+          </div>
+
+          <TextInput id="name" type="text" placeholder="Name" required />
+        </div>
+
+        <div>
+          <div className="mb-2 block">
+            <Label
+              htmlFor="email1"
+              value="Your email"
+              className="text-slate-300"
+            />
+          </div>
+
+          <TextInput
+            id="email1"
+            type="email"
+            placeholder="name@example.com"
+            required
+          />
+        </div>
+
+        <div>
+          <div className="mb-2 block">
+            <Label
+              htmlFor="file-upload"
+              value="Profile Image"
+              className="text-slate-300"
+            />
+          </div>
+
+          <FileInput id="file-upload" />
+        </div>
+
+        <div className="relative">
+          <div className="mb-2 block">
+            <Label
+              htmlFor="password1"
+              value="Password"
+              className="text-slate-300"
+            />
+          </div>
+
+          <TextInput
+            id="password1"
+            type={showPassword ? "text" : "password"}
+            placeholder="Create a password"
+            required
+          />
+
+          {showPassword ? (
+            <FaEye
+              className="absolute right-3 bottom-3 text-gray-500 text-xl cursor-pointer"
+              onClick={() => setShowPassword(!showPassword)}
+            />
+          ) : (
+            <FaEyeSlash
+              className="absolute right-3 bottom-3 text-gray-500 text-xl cursor-pointer"
+              onClick={() => setShowPassword(!showPassword)}
+            />
+          )}
+        </div>
+
+        <p className="mt-2 text-center">
+          Already have an account?{" "}
+          <Link
+            to="/login"
+            className="text-slate-400 hover:text-blue-400 duration-300"
+          >
+            login
+          </Link>
+        </p>
+
+        <Button type="submit" color="dark">
+          Register
+        </Button>
+      </form>
+    </div>
+  );
 }
