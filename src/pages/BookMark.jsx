74dd834cc7a1e78940674bbaaee6049ff9044827
Mojammel Noman <molla.noman23@gmail.com>
2024-11-07 22:21:05 +0600

    add bookmark page

--- src/pages/BookMark.jsx
+++ src/pages/BookMark.jsx
@@ -1,3 +1,16 @@
-export default function BookMark() {
-  return <div>BookMark</div>;
+import CardComponent from "../components/Card";
+
+export default function Bookmark() {
+  return (
+    <div className="mt-4">
+      <h1 className="text-3xl text-center italic mb-10">Bookmarked Blogs</h1>
+
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-7">
+        <CardComponent bookmark={true} />
+        <CardComponent bookmark={true} />
+        <CardComponent bookmark={true} />
+        <CardComponent bookmark={true} />
+      </div>
+    </div>
+  );
 }
